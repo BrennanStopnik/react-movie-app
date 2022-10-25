b06c3a2ae397df6b4117b6445db46cd7b67775d2
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-10-24 22:04:39 -0400

    Update everything.

---
+++ src/Components/MovieSidebar.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+
+
+const MovieSidebar = (props) => {
+	return (
+		<div className="movie-sidebar">
+			<h2>Movie Sidebar</h2>
+			{props.movieList.map((movieItem)=>{
+				return (
+					<Link to={`/movies/${movieItem.Title}`}>{movieItem.Title}</Link>
+				)
+			})}
+		</div>
+	)
+}
+
+export default MovieSidebar;
